4d117cd0a05ed81a1ff2a876ffc4248e413f3f2e
hamtos <135288715+hamtos@users.noreply.github.com>
2023-08-21 13:06:09 +0000

    [add]my_page

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,4 +1,37 @@
 class Public::CustomersController < ApplicationController
+  before_action :check_guest_account, only: [:update]
+
   def show
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "変更内容を保存しました"
+      redirect_to customers_my_page_path
+    else
+      render :show
+    end
+  end
+
+  def leave
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会しました"
+    redirect_to root_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:email, :name, :isdeleted)
+  end
+
+  def check_guest_account
+    if current_customer.email = 'guest@example.com'
+      flash[:error] = "ゲストアカウントの情報は変更できません。"
+      redirect_to customers_my_page_path
+    end
   end
 end
